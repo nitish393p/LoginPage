9d3714671d663815008a47dffaebc3c9e583cc30
Nitish <nitishdkpatil@gmail.com>
2024-12-11 16:36:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import LoginPage from "./components/LoginPage";
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <LoginPage />
+      </div>
+    </>
+  );
+}
